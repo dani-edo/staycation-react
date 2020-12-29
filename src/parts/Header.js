9fdfab85bc780d1bbcab64e27a0d4ce811e51cde
Dani Edo <dani.nur.afandi@gmail.com>
2020-12-29 13:34:57 +0700

    header compoent and bootstrap 4 setup

---
+++ src/parts/Header.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Button from "elements/Button";
+import BrandIcon from "parts/IconText";
+
+const contents = [
+  {
+    label: "Home",
+    link: "/",
+  },
+  {
+    label: "Browse by",
+    link: "/browse-by",
+  },
+  {
+    label: "Stories",
+    link: "/stories",
+  },
+  {
+    label: "Agents",
+    link: "/agents",
+  },
+];
+
+export default function Header(props) {
+  const getNavLinkClass = (path) => {
+    return props.location.pathname === path ? " active" : "";
+  };
+  return (
+    <header className="spacing-sm">
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <BrandIcon />
+
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ml-auto">
+              {contents.map((e, i) => {
+                return (
+                  <li className={`nav-item${getNavLinkClass(e.link)}`} key={i}>
+                    <Button className="nav-link" type="link" href={e.link}>
+                      {e.label}
+                    </Button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
