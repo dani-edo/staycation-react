45342edae9660a2b69b1faeee34fb1045f47fdc5
Dani Edo <dani.nur.afandi@gmail.com>
2021-01-02 08:18:59 +0700

    Detailspage and pagedetailtitle part

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "pages/LandingPage";
+import DetailsPage from "pages/DetailsPage";
 import Example from "pages/Example";
 import "assets/scss/style.scss";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Router>
         <Route exact path="/" component={LandingPage}></Route>
+        <Route exact path="/properties/:id" component={DetailsPage}></Route>
         <Route path="/example" component={Example}></Route>
       </Router>
     </div>
